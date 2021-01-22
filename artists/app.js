c3893d5f80f1b99ef2d3b62c3f90e4a485de237f
aukewor <60113380+aukewor@users.noreply.github.com>
2021-01-22 10:18:53 -0500

    /artists
    video page

---
+++ artists/app.js
@@ -0,0 +1,158 @@
+const DEBUG = true;
+
+function log(str) {
+  if (DEBUG) {
+    console.log(str);
+  }
+}
+
+
+// The list of painters and their videos.
+const painterVideoDict = {
+  'Vamba Bility': '503219177',
+  'David Craig': '503306710',
+  'Bri Brooks': '',
+  'Danielle De Jesus': '503314093',
+  'Nathaniel Donnett': '503307474',
+  'Leyla Faye': '503307928',
+  'Kate Meissner': '',
+  'Mich Miller': '503308625',
+  'Gabriel Mills': '503311373',
+  'Dala Nasser': '503196192',
+  'Alina Perez': '',
+  'Tamen Pérez': '503302531',
+  'Sara Rahmanian': '503313466',
+  'Jonathan Rajewski': '503312134',
+  'Emma Safir': '503316662',
+  'Chibuike Uzoma': '503315619',
+  'Curtis Welteroth': '503170693',
+}
+
+// Building a new array with just the video IDs from the painters.
+const videoIds = $.map(painterVideoDict, function(value, key) { return value });
+const horizontalVideos = ['503306710','503314093','503307474','503307928','503308625','503196192','503311373','503313466','503316662','503315619'];
+
+// jQuery stuff
+
+// For every painter in the list of painters, add a list item to the HTML
+$.each(Object.keys(painterVideoDict), function(idx, painter) {
+  // Create a new list item with the class `painter`, and the text of
+  // the painter's name.
+  var li = $('<li/>').addClass('painter').text(painter);
+
+  // If this is the first painter in the list (index == 0), then while
+  // want to make it "active" (apply styling) because the video is playing.
+  if (idx == 0) {
+    li.addClass('active');
+  }
+
+  if (painterVideoDict[painter].length == 0) {
+    li.addClass('no-media');
+  }
+
+  // Add the list item we just created to this list of painters in the HTML.
+  $('#painters').append(li);
+})
+
+// Takes in an index of which painter to highlight, and then does so
+function activatePainterName(index) {
+  // Remove the styling from all of the other names.
+  $('.painter').removeClass('active');
+  // Add the styling to just the name at the index.
+  $('.painter').eq(index).addClass('active');
+
+  if (horizontalVideos.includes(videoIds[index])) {
+    $('iframe').addClass('horizontal');
+  } else {
+    $('iframe').removeClass('horizontal');
+  }
+}
+
+function VimVids(el, options) {
+  options = options || {};
+  Object.assign(this, VimVids.options, options);
+  //this.playlist = Object.assign({}, VimVids.options.playlist, options.playlist);
+
+  this.currentVidIdx = 0
+  this.vidCount = this.playlist.length,
+
+  this.player = new Vimeo.Player(el, {
+    id: this.playlist[this.currentVidIdx],
+    width: this.width,
+    title: this.title,
+    muted: this.muted,
+    controls: this.controls,
+    autoplay: this.autoplay
+  });
+}
+
+VimVids.prototype = {
+  constructor : VimVids,
+
+  init() {
+    // this.player.setVolume(0);
+    this.player['play']()
+    this.onEnd()
+   },
+
+  loadVid(id) {
+    this.player.loadVideo(id)
+    this.player['play']()
+  },
+
+  onEnd() {
+    log("Video ended...")
+
+    this.player.on('ended', () =>  {
+      this.next()
+    });
+  },
+
+  next() {
+    log("Next video...")
+    this.currentVidIdx++;
+    if (this.currentVidIdx < this.vidCount){
+      this.currentVidIdx+1;
+    } else {
+      this.currentVidIdx = 0;
+    }
+    this.loadVid(this.playlist[this.currentVidIdx])
+
+    activatePainterName(this.currentVidIdx);
+  },
+
+  playAtIndex(index) {
+    if (index < this.vidCount) {
+      this.currentVidIdx = index;
+    } else {
+      this.currentVidIdx = 0;
+    }
+
+    this.loadVid(this.playlist[this.currentVidIdx]);
+  }
+}
+
+VimVids.options = {
+  loop: false,
+  title: false,
+  muted: false,
+  controls: false,
+  fullScreenToggle: false,
+  autoplay: true,
+  playlist: videoIds
+}
+
+let player = new VimVids('js-player')
+player.init()
+
+$('.painter').on('click', function() {
+  let index = $(this).index();
+  if (videoIds[index].length == 0) {
+    return;
+  }
+
+  activatePainterName(index)
+  player.playAtIndex(index)
+})
+
+
